f04fc78c755f0a355243ebdd52f822d66ca9c250
Cezary Nowak <czarekkawon@onet.pl>
2024-10-13 22:16:52 +0200

    AddedEditProfile

---
+++ BlogApp.Web/Pages/EditProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogApp.Web.Pages
+{
+    public class EditProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
