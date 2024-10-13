a05d8798f63e6923a88fca77542dc7a237f764af
Cezary Nowak <czarekkawon@onet.pl>
2024-10-13 22:17:12 +0200

    UpdatedProfilePageInGeneral

--- BlogApp.Web/Pages/Profile.cshtml.cs
+++ BlogApp.Web/Pages/Profile.cshtml.cs
@@ -23,7 +23,13 @@ namespace BlogApp.Web.Pages
         public List<Comment> Comments { get; set; }
 
         [BindProperty]
+        public WebPost WebPost { get; set; }
+
+        [BindProperty]
         public Comment Comment { get; set; }
+
+        [BindProperty]
+        public IFormFile Image { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -37,5 +43,61 @@ namespace BlogApp.Web.Pages
             .OrderByDescending(wp => wp.DateTimeOfPost)
             .ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostAddWebPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            WebPost.ApplicationUserId = user.Id;
+
+
+            var fileName = Path.GetFileNameWithoutExtension(Image.FileName);
+            var extension = Path.GetExtension(Image.FileName);
+            var newFileName = $"{fileName}_{DateTime.Now.Ticks}{extension}";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", newFileName);
+
+            if (!Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads")))
+            {
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads"));
+            }
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await Image.CopyToAsync(stream);
+            }
+
+
+            WebPost.ImagePath = $"/uploads/{newFileName}";
+
+
+            _context.WebPosts.Add(WebPost);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostAddCommentAsync(int webPostId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var webPost = await _context.WebPosts.FindAsync(webPostId);
+
+            Comment.ApplicationUserId = user.Id;
+            Comment.WebPostId = webPost.Id;
+
+            _context.Comments.Add(Comment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var post = await _context.WebPosts.FindAsync(id);
+
+            _context.WebPosts.Remove(post);
+            _context.SaveChanges();
+
+            return RedirectToPage();
+        }
+
     }
 }
